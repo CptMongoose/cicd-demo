5c7925eba3f529c32e380185df48c8fc9e97d854
CptMongoose <78185204+CptMongoose@users.noreply.github.com>
2025-12-13 04:18:14 +0000

    Add some funcs

--- src/app.py
+++ src/app.py
@@ -1,5 +1,22 @@
+import math
+
 def add(a,b):
     return a+b
 
 def sub(a,b):
     return a-b
+
+def mult(a,b):
+    return a*b
+
+def div(a,b):
+    return a/b
+
+def log(a):
+    return math.log(a)
+
+def sqaure(a):
+    return a*a
+
+def sin(a):
+    return math.sin(a)
